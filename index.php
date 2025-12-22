8fd48b2f50abec567d9f3841aab3548e1fd49e15
Cristian Kalid Rios Ayala <84214109+12kalid@users.noreply.github.com>
2025-12-22 16:58:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 $canal = isset($_GET['canal']) ? $_GET['canal'] : '';
 
+// Diccionario de canales actualizado
 $canales = [
-    "sicardi" => "https://vivo.solumedia.com:19360/sicarditv/sicarditv.m3u8"
+    "sicardi" => "https://vivo.solumedia.com:19360/sicarditv/sicarditv.m3u8",
+    "gentv"   => "https://srv3.zcast.com.br/gentv/gentv/playlist.m3u8"
 ];
 
 if (array_key_exists($canal, $canales)) {
     $url_final = $canales[$canal];
-    // Esta instrucción le dice a la TV que salte el bloqueo de origen
+    // Redireccionamos directamente a la fuente
     header("Location: $url_final");
     exit;
 } else {
     header("Content-Type: text/plain");
-    echo "#EXTM3U\n#EXTINF:-1,Canal No Encontrado";
+    echo "#EXTM3U\n#EXTINF:-1,Canal No Encontrado en leon8080";
 }
 ?>
